d5acb5979e51104e25a2b8d2cf07c5443e70eace
IffatMaab0 <iffatmaab11@gmail.com>
2025-06-24 06:45:07 +0500

    completed

--- agent.py
+++ agent.py
@@ -1,1 +1,19 @@
-import openai
+## first agent
+
+from groq import Groq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+start = Groq(api_key= os.getenv("GROQ_API_KEY"))
+
+
+response = start.chat.completions.create(
+    model ="llama3-70b-8192",
+    messages=[
+        {"role" : "user", "content":"tell about today's Weather in lahore"}
+    ]
+
+)
+
+print(response.choices[0].message.content)
