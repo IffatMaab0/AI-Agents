0cc67fd0e27716d737aa836ae87dfc4227a2e3db
IffatMaab0 <iffatmaab11@gmail.com>
2025-06-29 23:40:40 +0500

    updated

--- Agent_Langchain.py
+++ Agent_Langchain.py
@@ -1,1 +1,7 @@
 ## Modifying Agent Using Langchain
+
+import os
+from dotenv import load_dotenv
+import requests
+from langchain_core.tools import tool
+from langchain.agents import initialize_agent, AgentType
